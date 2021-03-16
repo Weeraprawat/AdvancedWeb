4b965729ea9c6513b7534060e319d0db561fb99c
Weeraprawat <pai_nigh55@hotmail.com.com>
2021-03-16 11:36:19 +0700

    update

---
+++ Lecture-07/test_employee.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import patch
+from employee import Employee
+
+
+class TestEmployee(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+
+    def setUp(self):
+        print('setup')
+        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_2 = Employee('Sue', 'Smith', 60000)
+
+    def tearDown(self):
+        print('tearDown\n')
+
+    def test_email(self):
+        print('test_email')
